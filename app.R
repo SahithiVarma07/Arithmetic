abe1fd7bb42fb3a993608cf265f947b903e9e327
Sahithi Uppalapati <your_github_email@example.com>
2025-10-02 13:08:54 -0500

    added multiply button

--- app.R
+++ app.R
@@ -1,29 +1,36 @@
 library(shiny)
 
 ui <- fluidPage(
-    titlePanel("Let's Do Arithmetic!"),
-    sidebarLayout(
-        sidebarPanel(
-            numericInput(inputId = "number1",
-                         label = "Number 1",
-                         value = 0),
-            numericInput(inputId = "number2",
-                         label = "Number 2",
-                         value = 0)
-        ),
-        mainPanel(
-            h2("The sum of the two numbers is:"),
-            textOutput("sum")
-        )
+  titlePanel("Let's Do Arithmetic!"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = "number1", label = "Number 1", value = 0),
+      numericInput(inputId = "number2", label = "Number 2", value = 0),
+      actionButton(inputId = "addBtn", label = "Add"),
+      actionButton(inputId = "multBtn", label = "Multiply")
+    ),
+    mainPanel(
+      h2("The result of the operation is:"),
+      textOutput("result")
     )
+  )
 )
-
 server <- function(input, output) {
-    output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
-    })
+  addResult <- eventReactive(input$addBtn, {
+    as.numeric(input$number1) + as.numeric(input$number2)
+  })
+  multResult <- eventReactive(input$multBtn, {
+    as.numeric(input$number1) * as.numeric(input$number2)
+  })
+  output$result <- renderText({
+    if (input$addBtn > 0 && input$addBtn >= input$multBtn) {
+      paste("Sum =", addResult())
+    } else if (input$multBtn > 0) {
+      paste("Product =", multResult())
+    } else {
+      "No calculation yet."
+    }
+  })
 }
 
 shinyApp(ui = ui, server = server)
